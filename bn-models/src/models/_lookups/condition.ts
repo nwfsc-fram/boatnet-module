c6244402c48d32127bb04b23e71d4f95af606548
Nicholas.Shaffer <nicholas.shaffer@noaa.gov>
2019-12-17 15:59:08 -0800

    added comment to condition animal code / description

--- bn-models/src/models/_lookups/condition.ts
+++ bn-models/src/models/_lookups/condition.ts
@@ -3,8 +3,8 @@ import { BaseLookup } from '../_base';
 export const ConditionTypeName = 'condition';
 
 export interface Condition extends BaseLookup {
-  animalCode?: string; 
-  animalDescription?: string;
+  animalCode?: string; // ashop code to delineate the type of animal, pulled from animal type lookup in norpac
+  animalDescription?: string; // ashop description of animal code used, pulled from animal type lookup in norpac
 
   legacy?: {
     conditionCode?: string;
