becdf1f951de954eca91b527f27bf52fb10019f1
melina.shak@noaa.gov <melina.shak@noaa.gov>
2019-12-17 14:21:19 -0800

    adding in examples for lookup docs

--- bn-models/src/models/_lookups/non-fishing-day-reason-code.ts
+++ bn-models/src/models/_lookups/non-fishing-day-reason-code.ts
@@ -4,3 +4,14 @@ export const nonFishingDayReasonCode = 'non-fishing-day-reason-code';
 
 export interface nonFishingDayReasonCode extends BaseLookup {
 }
+
+/* Example doc 
+{
+    "_id": "6ec983c88220cad5c56c5a94a37d2954",
+    "_rev": "1-d157ee618e2ab623b5cc55f37d078620",
+    "type": "nonFishingDayReason",
+    "description": "Steaming to/from fishing grounds",
+    "isActive": "true",
+    "isAshop": "true"
+}
+*/
