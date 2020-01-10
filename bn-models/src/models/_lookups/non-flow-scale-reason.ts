0b29e23bc319b0d8201daca2a0814e52eb6a00d1
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2020-01-10 14:03:59 -0800

    updating based on seth's comment

--- bn-models/src/models/_lookups/non-flow-scale-reason.ts
+++ bn-models/src/models/_lookups/non-flow-scale-reason.ts
@@ -3,7 +3,6 @@ import { BaseLookup } from '../_base';
 export const nonFlowScaleReasonCode = 'non-flow-scale-reason';
 
 export interface nonFlowScaleReasonCode extends BaseLookup {
-    reason: string
 }
 
 /* Example doc 
@@ -11,7 +10,7 @@ export interface nonFlowScaleReasonCode extends BaseLookup {
   "_id": "6ec983c88220cad5c56c5a94a3d8670f",
   "_rev": "2-13da8e51779e3b751dd7679d35d0dcf6",
   "type": "non-flow-scale-reason",
-  "reason": "CV DISCARD",
+  "description": "CV DISCARD",
   "isActive": "true",
   "isAshop": "true"
 }
