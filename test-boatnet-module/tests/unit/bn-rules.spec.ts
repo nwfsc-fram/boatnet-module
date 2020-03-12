a9d907802b116b388c237d47261cfd67c59d681f
Will Smith <will.smith@noaa.gov>
2020-03-12 11:53:15 -0700

    bn-rules: add basic test to test-boatnet-module

---
+++ test-boatnet-module/tests/unit/bn-rules.spec.ts
@@ -0,0 +1,60 @@
+import * as rules from '@boatnet/bn-rules';
+import { BoatnetBaseRule } from '@boatnet/bn-rules';
+
+describe('@boatnet/bn-rules', () => {
+  it('has a functional service', () => {
+    const testService = rules.rulesService
+    expect(testService).toBeTruthy();
+  });
+});
+
+// describe('@boatnet/bn-couch', () => {
+//   it('throws the correct errors if not initialized', () => {
+//     const testService = couch.couchService
+//     expect(testService).toBeTruthy();
+
+//     expect(() => {
+//       const master = testService.masterDB();
+//     }).toThrowError('Please log out');
+//     expect(() => {
+//       const lookups = testService.lookupsDB();
+//     }).toThrowError('Please log out');
+//     expect(() => {
+//       const user = testService.userDB();
+//     }).toThrowError('Please log out');
+//   });
+// });
+
+// describe('@boatnet/bn-couch', () => {
+//   it('returns not connected appropriately', () => {
+//     const testService = couch.couchService
+//     expect(testService).toBeTruthy();
+
+//     expect(testService.isConnected).toEqual(false);
+//   })
+// });
+
+// describe('@boatnet/bn-couch', () => {
+//   it('can basically connect', () => {
+//     const testService = couch.couchService
+//     expect(testService).toBeTruthy();
+
+//     // This will not actually connect.
+//     const credentials: CouchDBCredentials = {
+//       dbInfo: {
+//         urlRoot: 'https://localhost:5984',
+//         userDB: 'fake-user',
+//         masterDB: 'fake-master',
+//         lookupsDB: 'fake-lookups'
+//       },
+//       userCredentials: {
+//         username: 'test',
+//         password: 'test'
+//       }
+//     }
+//     expect ( () => {
+//       testService.connect(credentials)
+//     }).not.toThrowError();
+//     expect(testService.isConnected).toEqual(true);
+//   })
+// });
