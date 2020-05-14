420ffe6dc6cb76dbc33d56d90d01ba56b6510a68
Seth Gerou <seth.gerou@noaa.gov>
2020-05-14 12:34:57 -0700

    business rules module

---
+++ bn-business-rules/src/ifqaccounting/mortality-rates.ts
@@ -0,0 +1,27 @@
+/*
+
+Discard Mortality Rates
+
+Sablefish and lingcod discards are prorated based on a fleet-specific discard mortality rate. Pacific
+halibut discards are prorated based on the time the fish is on deck prior to discarding for bottom trawl
+vessels, while pot vessels have a fleet-specific discard mortality rate.
+
+Sablefish
+    • Midwater trawl - 100% mortality (no pro-ration needed)
+    • Pot - 20% mortality
+    • Bottom trawl - 50% mortality
+
+Lingcod
+    • Midwater trawl - 100% mortality (no pro-ration needed)
+    • Pot - 7% mortality
+    • Bottom trawl - 50% mortality
+
+Pacific halibut
+    • Midwater trawl - 100% mortality (no pro-ration needed)
+    • Pot - 18% mortality
+    • Bottom trawl - calculated based on time on deck. See Appendix D for details
+
+Note that all flat rate DMRs are applied regardless of the condition of the fish. That means that they are
+applied even to fish which may be in parts or otherwise obviously dead.
+
+*/
