bc964a8905be773e556d9092ec7c9f5d2cd21d97
Jim Fellows <57507677+jimfellows-NOAA@users.noreply.github.com>
2020-06-30 09:06:13 -0700

    Update ifq-grouping.ts
    adding comments for documentation.

--- bn-models/src/models/_common/ifq-grouping.ts
+++ bn-models/src/models/_common/ifq-grouping.ts
@@ -4,11 +4,16 @@ import { CatchGrouping } from '../_lookups/catch-grouping';
 
 export const IfqGroupingTypeName = 'ifq-grouping';
 
+/* 
+used to translate species/grouping to IFQ management grouping for in-season VAS reporting and expansions
+Example:
+  Sablefish (4fa0ab1f2ccf04160ab89da3e3b14d77) caught at latitude 40.266667 in 2020 --> "Sablefish north"
+*/
 export interface IfqGrouping extends Base {
-  ifqGrouping?: string;
-  regulationYear?: number;
-  upperLatitude?: number;
-  lowerLatitude?: number;
-  groupings?: CatchGrouping[];
-  taxonomies?: Taxonomy[];
+  ifqGrouping?: string; // name used by IFQ for mgmt groupings, see IFQ_SPECIE_GROUPINGS in ORACLE
+  regulationYear?: number; // groupings will be refreshed each year, possible they can change
+  upperLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
+  lowerLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
+  groupings?: CatchGrouping[]; // array w. all related groupings
+  taxonomies?: Taxonomy[]; // array w. all related taxonomies
 }
