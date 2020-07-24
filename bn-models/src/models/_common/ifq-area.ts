da76c05ffc23242efe2d65f663080ac79561d11e
Jim Fellows <57507677+jimfellows-NOAA@users.noreply.github.com>
2020-07-24 11:29:32 -0700

    ifq-area / grouping update
    adding ifq-area interface to nest within ifq-grouping, and altering grouping fields

---
+++ bn-models/src/models/_common/ifq-area.ts
@@ -0,0 +1,16 @@
+import { Base } from '../_base/base';
+import { Taxonomy } from '../_lookups/taxonomy';
+import { CatchGrouping } from '../_lookups/catch-grouping';
+
+export const IfqAreaTypeName = 'ifq-area';
+
+export interface IfqArea extends Base {
+  upperLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
+  lowerLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
+  groupings?: CatchGrouping[]; // array w. all related groupings
+  taxonomies?: Taxonomy[]; // array w. all related taxonomies
+  legacy?: {
+    fishingAreaIds: number[];
+    ifqSpeciesGroupingIds: number[];
+  };
+}
--- bn-models/src/models/_common/ifq-grouping.ts
+++ bn-models/src/models/_common/ifq-grouping.ts
@@ -1,6 +1,7 @@
 import { Base } from '../_base/base';
 import { Taxonomy } from '../_lookups/taxonomy';
 import { CatchGrouping } from '../_lookups/catch-grouping';
+import { IfqArea } from './ifq-area';
 
 export const IfqGroupingTypeName = 'ifq-grouping';
 
@@ -8,12 +9,10 @@ export const IfqGroupingTypeName = 'ifq-grouping';
 used to translate species/grouping to IFQ management grouping for in-season VAS reporting and expansions
 Example:
   Sablefish (4fa0ab1f2ccf04160ab89da3e3b14d77) caught at latitude 40.266667 in 2020 --> "Sablefish north"
+  IfqAreas array will store latitude bounds and taxonomies / groupings that apply
 */
 export interface IfqGrouping extends Base {
-  ifqGrouping?: string; // name used by IFQ for mgmt groupings, see IFQ_SPECIE_GROUPINGS in ORACLE
+  groupName?: string; // name used by IFQ for mgmt groupings, see IFQ_SPECIE_GROUPINGS in ORACLE
   regulationYear?: number; // groupings will be refreshed each year, possible they can change
-  upperLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
-  lowerLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
-  groupings?: CatchGrouping[]; // array w. all related groupings
-  taxonomies?: Taxonomy[]; // array w. all related taxonomies
+  regulationAreas?: IfqArea[];
 }
