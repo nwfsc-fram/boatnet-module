98bd9fb74fa85dd2487dd773949bbc3d564c0687
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2020-09-14 14:45:00 -0700

    adding comments back in

--- bn-models/src/models/_lookups/deterrent-type.ts
+++ bn-models/src/models/_lookups/deterrent-type.ts
@@ -2,3 +2,5 @@ import { BaseLookup } from '../_base';
 export const DeterrenceTypeTypeName = 'deterrence-type';
 
 export type DeterrentType = BaseLookup;
+// notes:
+// description| seal bomb, firearm, gaff, yelling, acoustic device, other
--- bn-models/src/models/_lookups/waiver-type.ts
+++ bn-models/src/models/_lookups/waiver-type.ts
@@ -3,3 +3,7 @@ import { BaseLookup } from '../_base';
 export const WaiverTypeTypeName = 'waiver-type';
 
 export type WaiverType = BaseLookup;
+// notes:
+// description?: string; // Trip, Selection Cycle, Coverage Period
+// Selection Cycle = remainder of the year, remainder of 6-month cycle
+// lookupVal?: number; // T, SC, CP
