4d18aff162c5b5dce3e27d8bc9128a519bc15eba
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2020-09-24 17:06:05 -0700

    forgot to add this as well

--- bn-expansions/src/EM/helpers/maps.ts
+++
@@ -1,9 +0,0 @@
-export const groupingsToSpeciesDummyMap = {
-    "5000": ["DOVR", "EFLS"],
-    "shak": [
-        {
-            speciesCode: "21",
-            name: "Who cares"
-        }
-    ]
-}
