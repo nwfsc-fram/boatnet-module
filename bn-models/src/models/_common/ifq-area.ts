dda6829a127408215a692f729f2b2de1df165930
Jim Fellows <57507677+jimfellows-NOAA@users.noreply.github.com>
2020-09-28 08:53:10 -0700

    swap taxonomy-alias in for taxonomy in ifq-grouping
    changing to taxonomy-alias in ifq-area model, which will make taxonomy-alias avaiable in ifq-grouping docs.

--- bn-models/src/models/_common/ifq-area.ts
+++ bn-models/src/models/_common/ifq-area.ts
@@ -1,5 +1,5 @@
 import { Base } from '../_base/base';
-import { Taxonomy } from '../_lookups/taxonomy';
+import { TaxonomyAlias } from '../_lookups/taxonomy-alias';
 import { CatchGrouping } from '../_lookups/catch-grouping';
 
 export const IfqAreaTypeName = 'ifq-area';
@@ -8,7 +8,7 @@ export interface IfqArea extends Base {
   upperLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
   lowerLatitude?: number; // upper lat. bound for grouping, replaces FISHING_AREA code
   catchGroupings?: CatchGrouping[]; // array w. all related groupings
-  taxonomies?: Taxonomy[]; // array w. all related taxonomies
+  taxonomyAliases?: TaxonomyAlias[]; // array w. all related taxonomyAliases
   legacy?: {
     fishingAreaIds: number[];
     ifqSpeciesGroupingIds: number[];
