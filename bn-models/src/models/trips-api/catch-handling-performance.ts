9089889dabc2a484dd6537ed87c7f0a06cf20a07
Seth Gerou <seth.gerou@noaa.gov>
2020-10-29 14:51:36 -0700

    em: add misc missing models

---
+++ bn-models/src/models/trips-api/catch-handling-performance.ts
@@ -0,0 +1,9 @@
+import { BaseLookup } from '../_base/base-lookup';
+export const CatchHandlingPerformanceTypeName = 'catch-handling-performance';
+
+export type CatchHandlingPerfrmance = BaseLookup;
+    // description
+    // lookupValue
+    // isActive
+    // isEm
+
---
+++ bn-models/src/models/trips-api/em-source.ts
@@ -0,0 +1,9 @@
+import { BaseLookup } from '../_base/base-lookup';
+export const EMSourceTypeName = 'em-source';
+
+export type EMSource = BaseLookup;
+    // description
+    // lookupValue
+    // isActive
+    // isEm
+
