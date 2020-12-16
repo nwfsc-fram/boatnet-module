2ee3209443852315ffb6e41a0ea629dc6c3eb62a
Seth Gerou <seth.gerou@noaa.gov>
2020-12-16 08:53:10 -0800

    bn-models: fishery, person-alias, and ots-target documentation

--- bn-models/src/models/_lookups/person-alias.ts
+++ bn-models/src/models/_lookups/person-alias.ts
@@ -4,12 +4,31 @@ import { CouchID } from '../_common';
 export const PersonAliasTypeName = 'person-alias';
 
 export interface PersonAlias extends BaseLookup {
-    // uses isAshop, etc. booleans from BaseLookup
-    // uses isActive boolean
     firstName?: string;
     lastName?: string;
     personDocId?: CouchID;
-    userName?: string; // email address
+    userName?: string; // email address (minus noaa.gov for noaa, includes @domain.something for external)
     roles?: string[]; // observer, captain, observer-staff, delegate, etc.
     debriefer?: CouchID;
+    // isActive: boolean
+    // isWcgop: boolean
+    // isAshop: boolean
 }
+
+    /*
+        {
+            type: "person-alias",
+            firstName: "Bill",
+            lastName: "Wilson"
+            personDocId: "6bd01c3b39ff036c69rd288d3e2c2582",
+            userName: "bill.wilson"
+            isActive: true,
+            isWcgop: true,
+            roles: [
+                "debriefer",
+                "staff",
+                "observer"
+            ],
+            debriefer: "5qd01c3b39ff036c69rd288d3e2c2582"
+        }
+    */
--- bn-models/src/models/wcgop/ots-target.ts
+++ bn-models/src/models/wcgop/ots-target.ts
@@ -6,13 +6,55 @@ import { Vessel } from '../_lookups';
 export const OTSTargetTypeName = 'ots-target';
 
 export interface OTSTarget extends Base {
-  fishery: string;
-  targetType: string;
-  targetVessel?: Vessel;
-  targetPortGroup?: any;
-  coverageGoal?: number;
-  setRate?: number;
-  effectiveDate?: BoatnetDate | undefined;
-  expirationDate?: BoatnetDate | undefined;
-  status?: string;
+  fishery: string; // target is specific to a fishery, and may be scoped to a vessel or portGroup
+  targetType: string;  // fishery, vessel, or portGroup
+  targetVessel?: Vessel; // used if targetType is 'vessel'
+  targetPortGroup?: any; // used if targetType is 'portGroup'
+  coverageGoal?: number; // target % of trips within targetType to cover
+  setRate?: number; // % number used in calculating whether to cover a trip (number used to achieve coverageGoal)
+  effectiveDate?: BoatnetDate | undefined; // future targets can be set in advance, so they're given a date range
+  expirationDate?: BoatnetDate | undefined; // last date the target is valid
+  status?: string; // when a new ots-target supercedes an old one, the old target's status is set to "Inactive"
+                        // Older ots-target docs may have status Active with an expirationDate in the past
 }
+
+/*
+{
+  type: "ots-target",
+  fishery: "Electronic Monitoring EFP",
+  coverageGoal: "35",
+  setRate: "40",
+  effectiveDate: "2019-05-30T20:38:18-07:00",
+  exprirationDate: "2020-01-01T20:38:18-07:00",
+  status: "Active",
+  targetType: "vessel",
+  targetVessel: {
+    _id: "d92cbndghnghe2dc5056a65c",,
+    type: "vessel",
+    port: {
+      _id: "d92gh4f8fcvbngh92e2dc501c7edb",
+      type: "port",
+      name: "ASTORIA / WARRENTON",
+      code: "AST",
+      group: "AT",
+      state: "OR",
+      createdBy: 1180,
+      createdDate: "2000-01-03T13:54:59-08:00",
+      updatedBy: "Bill Wilson",
+      updatedDate: "2019-05-02T08:11:22-07:00"
+    },
+    vesselName: "Fish Master 3000",
+    vesselType: {
+      description: "OA Fixed Gear",
+      _id: "d92c555184bnfghn92e2dc5013e704"
+    },
+    coastGuardNumber: "543214",
+    registeredLength: {
+      measurementType: "length",
+      value: 56,
+      units: "FT"
+    }
+  }
+}
+
+*/
