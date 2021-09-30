5438d272caca04fdc6fe85fbc728c3d622de6f53
Melina <44685558+melinashak-noaa@users.noreply.github.com>
2021-09-30 14:46:18 -0700

    updating mongo client

--- bn-mongo/src/_services/mongo.service.ts
+++ bn-mongo/src/_services/mongo.service.ts
@@ -1,34 +1,92 @@
+import { triggerAsyncId } from 'async_hooks';
 import request from 'request';
-import { authService } from '@boatnet/bn-auth';
 
-class MongoService {
-    url: string;
-    token: string | undefined;
+export class MongoService {
+    url: string = '';
+    token: string = '';
 
     constructor() {
-        this.url = authService.getTripsApiUrl() + '/api/v1/mongo';
-        this.token = authService.getCurrentUser()!.jwtToken;
+        console.log('Mongo service created')
     }
 
-    read(database: string, collection: string, query: any) {
+    connect(url: string, token: string) {
+        this.url = url;
+        this.token = token;
+    }
+
+    read(database: string, collection: string, query: any, options?: any) {
         return new Promise( (resolve, reject) => {
-            let queryString = '';
-            Object.keys(query).forEach( (key: string) => {
-                if (Object.keys(query).indexOf(key) === 0) {
-                    queryString += '?';
+            const queryOptions = options ? options : {};
+            const payload: any = {
+                query,
+                options: queryOptions
+            };
+            const queryUrl = this.url + '/' + database + '/' + collection;
+            request.post(
+                {
+                    url: queryUrl,
+                    json: true,
+                    headers: {
+                        'authorization': 'Token ' + this.token,
+                        'Content-Type': 'application/json'
+                    },
+                    body: payload
+                }, (err: any, response: any, body: any) => {
+                    if (!err && response.statusCode === 200) {
+                        resolve(body);
+                    } else {
+                        reject(body);
+                    }
                 }
-                if (key) {queryString += key + '=' + query[key]; }
-                if (Object.keys(query).indexOf(key) > 0) {
-                    queryString += '&';
+            );
+        });
+    }
+
+    mongoAggregate(database: any, collection: string, pipeline: any) {
+        return new Promise( (resolve, reject) => {
+            const payload = {pipeline};
+            const queryUrl = this.url + '/aggregate/' + database + '/' + collection;
+            request.post(
+                {
+                    url: queryUrl,
+                    json: true,
+                    headers: {
+                        'authorization': 'Token ' + this.token,
+                        'Content-Type': 'application/json'
+                    },
+                    body: payload
+                }, (err: any, response: any, body: any) => {
+                    if (!err && response.statusCode === 200) {
+                        resolve(body);
+                    } else {
+                        reject(body);
+                    }
                 }
-            });
-            const queryUrl = this.url + '/' + database + '/' + collection + queryString;
+            );
+        });
+    }
+
+    // example query:
+    // const result = await mongoAggregate(
+    //     'boatnetdb',
+    //     'lookups.beauforts',
+    //     [
+    //         {$match: {isActive: true}},
+    //         {$limit: 3}
+    //     ]
+    // );
+    // console.log(result);
+
+    mongoGetOne(database: any, collection: string, id: string) {
+        return new Promise( (resolve, reject) => {
+            const queryUrl = this.url + '/get/' + database + '/' + collection + '/' + id;
             request.get(
                 {
                     url: queryUrl,
                     json: true,
                     headers: {
-                        authorization: 'Token ' + this.token
+                        'authorization': 'Token ' + this.token,
+                        'Content-Type': 'application/json'
                     }
                 }, (err: any, response: any, body: any) => {
                     if (!err && response.statusCode === 200) {
@@ -41,7 +99,48 @@ class MongoService {
         });
     }
 
-    write(documents: object[]) {
+    // example query:
+    // const getOneResult = await mongoGetOne(
+    //     'boatnetdb',
+    //     'trips_api.trips',
+    //     "612515f92feb772abc836e6c"
+    // )
+    // console.log(getOneResult);
+
+    mongoGetMany(database: any, collection: string, ids: string[]) {
+        return new Promise( (resolve, reject) => {
+            const queryUrl = this.url + '/getMany/' + database + '/' + collection + '/';
+            request.post(
+                {
+                    url: queryUrl,
+                    json: true,
+                    headers: {
+                        'authorization': 'Token ' + this.token,
+                        'Content-Type': 'application/json'
+                    },
+                    body: {
+                        ids
+                    }
+                }, (err: any, response: any, body: any) => {
+                    if (!err && response.statusCode === 200) {
+                        resolve(body);
+                    } else {
+                        reject(body);
+                    }
+                }
+            );
+        });
+    }
+
+    // example query:
+    // const getManyResult = await mongoGetMany(
+    //     'boatnetdb',
+    //     'trips_api.trips',
+    //     ["612515f92feb772abc836e6c"]
+    // )
+    // console.log(getManyResult)
+
+    mongoWrite(documents: object[]) {
         return new Promise( (resolve, reject) => {
             const queryUrl = this.url;
             request.post(
@@ -62,8 +161,8 @@ class MongoService {
             );
         });
     }
-    
-    update(document: any) {
+
+    mongoUpdate(document: any) {
         return new Promise( (resolve, reject) => {
             const queryUrl = this.url;
             request.put(
@@ -84,8 +183,8 @@ class MongoService {
             );
         });
     }
-    
-    delete(document: any) {
+
+    mongoDelete(document: any) {
         return new Promise( (resolve, reject) => {
             const queryUrl = this.url;
             request.delete(
